45f6454018f674677066718d333209e8ae912de1
NguyenToi2162001 <Nguyentamtoi216@gmail.com>
2024-09-09 19:56:15 +0700

    toi

---
+++ js/menu.js
@@ -0,0 +1,34 @@
+var items = document.querySelectorAll(".item");
+console.log(items);
+var listmenu = document.querySelector(".list-menu")
+document.querySelector(".arrow-left").addEventListener("click",() =>{
+    items.forEach(items =>{
+        items.classList.toggle("hidden")
+        
+    })
+    listmenu.classList.toggle("list-menu")
+    
+})
+var lists = document.querySelectorAll(".list");
+var boxs = document.querySelectorAll(".box")
+console.log(lists);
+
+console.log(boxs);
+
+lists.forEach((item,index) => {
+    item.addEventListener("click", () => {
+        lists.forEach(item => item.style.color = "white");
+        item.style.color = "black";
+        boxs.forEach(item => item.style.display= "none")
+        boxs[index].style.display = "block";
+    })
+   
+})
+
+var drops = document.querySelector(".drop")
+
+document.querySelector(".user").addEventListener("click",() =>{
+    drops.classList.toggle("hiddens")
+    
+})
+
