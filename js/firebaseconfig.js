a1553f9db173a01b44a264d4bfc7401fad02710e
NguyenToi2162001 <Nguyentamtoi216@gmail.com>
2024-09-27 19:24:12 +0700

    toi

---
+++ js/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPV-vR3w9kdu1TTTn8w_D-2_tKpPCHr3U",
+  authDomain: "nguyentoi.firebaseapp.com",
+  projectId: "nguyentoi",
+  storageBucket: "nguyentoi.appspot.com",
+  messagingSenderId: "329969094572",
+  appId: "1:329969094572:web:7929c7a7d7d2f37a127409",
+  measurementId: "G-YHHS9ZQZE5"
+};
+
+firebase.initializeApp(firebaseConfig);
--- js/getFood.js
+++ js/getFood.js
@@ -1,7 +1,8 @@
 getAll(urlFood, displayFood);
+const storage = firebase.storage();
 let listFood;
 function displayFood(data) {
-  listFood=data;
+  listFood = data;
   const food = document.querySelector(".foods");
   data.forEach((value) => {
     let item = document.createElement("div");
@@ -10,7 +11,12 @@ function displayFood(data) {
                                         <div class="name d-flex">
                                             <p>${value.id}</p>
                                             <h3>${value.nameFood}</h3>
+                                        
+                                        <div>
+                                            <i onclick="deletefood(${value.id}, '${value.img}')" data-bs-toggle="modal" data-bs-target="#deleteFood" class="fa-solid fa-trash-can me-2"></i>
                                             <i class="fa-solid fa-pen-to-square"></i>
+                                        </div>
+                                       
                                         </div>
                                         <img class="mt-3" src="${value.img}">
                                         <div class="card-body">
@@ -27,17 +33,83 @@ function displayFood(data) {
     let minus = item.querySelector(".fa-minus");
     let plus = item.querySelector(".fa-plus");
     let input = item.querySelector("input");
-    
-    
+
+
     minus.addEventListener("click", () => {
       if (input.value > 0) {
         input.value = parseInt(input.value) - 1;
       }
     }
     )
-    plus.addEventListener("click", () =>{
+    plus.addEventListener("click", () => {
       input.value = parseInt(input.value) + 1;
     })
 
-})}
+  })
+}
+document.getElementById("image-food").addEventListener("change", (elemen) => {
+  const file = elemen.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById("img-food").src = e.target.result;
+    }
+    reader.readAsDataURL(file);
+  }
+})
+document.getElementById("add-food").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const price = document.getElementById("price").value;
+  const imgUrl = await uploadImage();
+  const newFood = {
+    nameFood: name,
+    img: imgUrl,
+    price: price
+  }
+  add(urlFood, newFood);
+})
+function uploadImage() {
+  return new Promise((resolve, reject) => {
+    const file = document.getElementById("image-food").files[0];
+    if (file) {
+      const imgPath = "images/" + file.name;
+      const storageRef = storage.ref(imgPath);
+      const uploadTask = storageRef.put(file);
 
+      uploadTask.on(
+        "state_changed",
+        null,
+        (error) => {
+          reject(error); // Handle errors
+        },
+        () => {
+          uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+            resolve(downloadURL); // Resolve the promise with the downloadURL
+          });
+        }
+      );
+    } else {
+      reject(new Error("No file selected"));
+    }
+  });
+}
+function deleted(url,id) {
+  fetch(`${url}/${id}`, {
+      method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(data => {      
+    })
+    .catch(error => console.error('Lỗi khi cập nhật đơn hàng', error));
+}
+let idxoa;
+function deletefood(id,img) {
+  idxoa = id;
+  document.getElementById("img-delete").src = img ;
+  
+}
+
+document.getElementById("xoa-food").addEventListener("click",  () => {
+  deleted(urlFood,idxoa);
+})
